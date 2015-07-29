e1a355917e2d89b6471cc3bb7f4fe303c2f23f69
ChanWoo Jeong <enghqii@gmail.com>
2015-07-29 22:50:22 +0900

    VM: Added simple VirtualMachine, and main func

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,31 @@
+#include <iostream>
+
+extern int yyparse(void);
+extern FILE *yyin;
+
 int main(int argc, char** argv) {
+
+    if (argc < 2) {
+
+        std::cout << "missing input file" << std::endl;
+        return -1;
+
+    }
+    else {
+
+        FILE *input_file = fopen(argv[1], "r");
+
+        if(!input_file) {
+            printf("can't open file\n");
+            return -1;
+        }
+
+        yyin = input_file;
+        if(yyparse()) {
+            fprintf(stderr, "Error!\n");
+            exit(1);
+        }
+    }
+
     return 0;
 }
