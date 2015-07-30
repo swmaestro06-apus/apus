df22eeb3706274a88ff7c9a8f5257c28b79104ba
JeongMinCha <cjm9236@naver.com>
2015-07-31 00:40:12 +0900

    Setting better hierarchy

---
+++ src/common/common.cpp
@@ -0,0 +1,23 @@
+#include "common/common.h"
+
+int TypeLength (int type) {
+    int length = 0;
+    switch (type) {
+       case U8:S8:C8:
+           length = 1; 
+           break;
+       case U16:S16:C16:
+           length = 2;
+           break;
+       case U32:S32:C32:F32:
+           length = 4;
+           break;
+       case U64:S64:C64:F64:
+           length = 8;
+           break;
+       default:
+           length = 0;
+           break;
+    }
+    return length;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-using namespace std;
-
 int main() {
-	cout << "Hello, World!" << endl;
 	return 0;
 }
