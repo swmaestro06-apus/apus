db49e15c1262c286bc8d010c1e34b93610ea5724
JeongMinCha <cjm9236@naver.com>
2015-08-03 17:54:15 +0900

    Add 'common' files
    include/ src/ 폴더 아래에 common.h, common.cpp 파일을 작성하였습니다. 이
    파일들은 공통적으로 사용되는 매크로, 함수, 변수 등을 지원하고자 작성된
    것입니다.

---
+++ src/common/common.cpp
@@ -0,0 +1,47 @@
+#include "common/common.h"
+#include <iostream>
+using namespace std;
+
+int TypeLength (VarType type) {
+    int length = 0;
+    switch (type) {
+        case U8:
+        case S8:
+        case C8:
+            length = 1; // 1 byte
+            break;
+            
+        case U16:
+        case S16:
+        case C16:
+            length = 2; // 2 bytes
+            break;
+            
+        case U32:
+        case S32:
+        case C32:
+        case F32:
+            length = 4; // 4 bytes
+            break;
+            
+        case U64:
+        case S64:
+        case F64:
+            length = 8; // 8 bytes
+            break;
+            
+        case STR8:
+        case STR16:
+        case STR32:
+            length = 0; // not fixed. variable-length
+            break;
+            
+            // exception
+        default:
+            cout << "There's no matched variable type." << endl;
+            length = -1;
+            break;
+    }
+    return length;
+}
+
