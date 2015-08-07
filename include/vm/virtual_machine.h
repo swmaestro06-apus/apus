04c583e6bc151ad1c27afdfa6f16ca6ec343412e
ChanWoo Jeong <enghqii@gmail.com>
2015-08-08 03:18:58 +0900

    VM: Added simple implementation of VirtualMachine::Run()
    앞서 만든 Statement등의 클래스를 사용해서 Run 멤버 함수의 인터페이스를 수정했습니다.

--- include/vm/virtual_machine.h
+++ include/vm/virtual_machine.h
@@ -1,18 +1,24 @@
 #ifndef VIRTUALMACHINE_H
 #define VIRTUALMACHINE_H
 
+#include <vector>
+#include <memory>
 #include "vm/context.h"
 
 namespace apus {
+
+    class Context;
+    class Statement;
 
     class VirtualMachine {
     public:
         VirtualMachine();
         virtual ~VirtualMachine();
 
-        void Run();
+        void Run(std::vector<std::shared_ptr<apus::Statement>> instructions);
 
     private:
+        Context* context_;
     };
 
 }
--- src/vm/virtual_machine.cpp
+++ src/vm/virtual_machine.cpp
@@ -1,5 +1,6 @@
 #include "vm/virtual_machine.h"
 #include "vm/context.h"
+#include "ast/statement/statement.h"
 
 namespace apus {
 
@@ -11,7 +12,10 @@ namespace apus {
         delete context_;
     }
 
-    void VirtualMachine::Run() {
+    void VirtualMachine::Run(std::vector<std::shared_ptr<apus::Statement>> instructions) {
+        for (std::shared_ptr<apus::Statement> stmt : instructions) {
+            stmt->Execute(*context_);
+        }
     }
 
 }
