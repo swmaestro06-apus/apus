932c4593593f3fc7a6d2c1ccbda157b66f2228f4
JeongMinCha <cjm9236@naver.com>
2015-08-11 15:20:13 +0900

    BinaryReader: Implementation of BinaryReader
    BinaryReader 클래스의 멤버변수와 메서드들을 구현하였습니다.
    
    멤버변수
    BinaryReader 클래스는 파일이름과 파일을 읽기 위해서 사용하는 파일 스트림 객체를 멤버변수로서 갖고 있습니다.
    
    메서드
    BinaryReader 클래스는 바이너리 파일의 특정 위치에서 특정 타입에 대응하는 바이트 크기만큼 바이트를 읽어들이고, 그 값을 그 타입에 맞게 출력합니다. 그 출력값은 argument를 통해서 전달하도록 구현이 되어 있고, 반환 값은 read 성공 여부를 반환합니다. 함수가 제대로 된 argument를 전달 받았고, 함수 내에서 문제없이 작동을 한 경우, 0을 반환하고, 실패한 경우 -1을 반환하도록 구현이 되어 있습니다.

---
+++ include/utils/BinaryReader.h
@@ -0,0 +1,33 @@
+#ifndef __BINARY_READER_H__
+#define __BINARY_READER_H__
+
+#include <iostream>
+#include <fstream>
+#include "common/common.h"
+using namespace std;
+
+class BinaryReader {
+public:
+    BinaryReader ();
+    BinaryReader (string& file_name);
+    ~BinaryReader ();
+
+    int ReadInt (streampos offset, VarType type, uint64_t& out_int);
+    int ReadInt (VarType type, uint64_t& out_int);
+
+    int ReadReal (streampos offset, VarType type, double& out_real);
+    int ReadReal (VarType type, double& out_real);
+
+    int ReadChar (streampos offset, VarType type, uint32_t& out_char);
+    int ReadChar (VarType type, uint32_t& out_char);
+
+    int ReadString (streampos offset, VarType type, string& out_str);
+    int ReadString (VarType type, string& out_str);
+
+private:
+    string file_name_;                  // file name
+    ifstream file_stream_;              // file stream to read
+};
+
+#endif
+
