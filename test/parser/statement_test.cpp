36f8d7f879a487287ab40a8b6f98064d4d018cde
Kim Jung Hyun <junghyun153@naver.com>
2015-08-13 20:04:43 +0900

    parser: Added statement test code
    Added 6 kind of test
     - for_stmt
     - if_stmt
     - expr_stmt
     - var_def_stmt
     - block_stmt
     - jump_stmt
    
    Now, it can test only corrected source code

---
+++ test/parser/statement_test.cpp
@@ -0,0 +1,112 @@
+#include "gtest/gtest.h"
+
+extern int yyparse(void);
+extern int yy_scan_string(const char *);
+
+// test_x x number is defined sub test
+// 1 : corrected test
+// 2 : uncorrected test
+// 3 : CR test
+
+static char var_def_test_1[] = "\
+var u8 a                \n\
+var u8 b = 10           \n\
+var c8 c = 'a'          \n\
+var str d = \"sss\"     \n\
+var f32 e = 3.4 + 3.7   \n\
+";
+static char jump_stmt_test_1[] = "\
+continue        \n\
+break           \n\
+return          \n\
+return 10+10    \n\
+exit            \n\
+exit -1         \n\
+";
+static char for_stmt_test_1[] = "\
+for(i = (32+8)*3/4<<2; i < 10; i += 1) { }  \n\
+for(i=10; ; ) { }                           \n\
+for( ; i < 10; ) { }                        \n\
+for( ; ; i += 1) { }                        \n\
+for( ; ; ) { }                              \n\
+";
+static char if_stmt_test_1[] = "\
+if(i < 10) { }      \n\
+if(i == 10) {       \n\
+    a = 10          \n\
+}                   \n\
+else {              \n\
+    a = 0           \n\
+}                   \n\
+if(i != 10) {       \n\
+    a = 10          \n\
+}                   \n\
+else if(i == 10) {  \n\
+    a = 0           \n\
+}                   \n\
+else {              \n\
+    a = 1           \n\
+}                   \n\
+";
+static char expr_stmt_test_1[] = "\
+3 + 4   \n\
+3 << 4  \n\
+3 ^ 4   \n\
+!4      \n\
+~4      \n\
+";
+static char block_stmt_test_1[] = "\
+{   var u8 a = 7        \n\
+    if(a=1) { }         \n\
+    else if(b=1) { }    \n\
+    else { }            \n\
+    for( ; ; ) { }      \n\
+    3 * 9               \n\
+    return 9            \n\
+    exit                \n\
+}                       \n\
+";
+static char if_stmt_test_3[] = "\
+if                      \n\
+( i == 10 )             \n\
+{                       \n\
+    var u8 a = 10       \n\
+} else if ( j == 10 ) { \n\
+    a = 10              \n\
+}                       \n\
+                        \n\
+else { }                \n\
+";
+
+TEST (ParserTest, StmtCorrectTest) {
+    yy_scan_string(var_def_test_1);
+    int result = yyparse();
+    EXPECT_EQ (result, 0);
+
+    yy_scan_string(jump_stmt_test_1);
+    result = yyparse();
+    EXPECT_EQ (result, 0);
+
+    yy_scan_string(for_stmt_test_1);
+    result = yyparse();
+    EXPECT_EQ (result, 0);
+
+    yy_scan_string(if_stmt_test_1);
+    result = yyparse();
+    EXPECT_EQ (result, 0);
+
+    yy_scan_string(expr_stmt_test_1);
+    result = yyparse();
+    EXPECT_EQ (result, 0);
+
+    yy_scan_string(block_stmt_test_1);
+    result = yyparse();
+    EXPECT_EQ (result, 0);
+}
+
+TEST (ParserTest, StmtLineTest) {
+    int result;
+    yy_scan_string(if_stmt_test_3);
+    result = yyparse();
+    EXPECT_EQ (result, 0);
+}
