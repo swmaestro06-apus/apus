3b96c8d0f46e2b3055067a99d5e2bf17cd4bd4b1
Kim Jung Hyun <junghyun153@naver.com>
2015-08-21 02:21:12 +0900

    parser: Enabled yyparse function reenterable
    All of test_code and apus.y files Added "parser_context.h"
      and yyparse() modified yyparse(&argument).
    
    Now, yyparse() can deliver arguments.
    
    If you deliver arguments in yyparse function,
      you must add yyerror arguments.
    
    ex)
      yyparse(&pctx);
    
      yyerror(ParserContext* pctx, char *str) { }

--- test/parser/array_test.cpp
+++ test/parser/array_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
+#include "parser_context.h"
 
-extern int yyparse(void);
+extern int yyparse(apus::ParserContext* pctx);
 extern int yy_scan_string(const char *);
 
 // test_x x number is defined sub test
@@ -42,28 +43,30 @@ var struct id2[2][2][2] ll = [[[{1, 2}, {3, 4}], [{5, 6}, {7, 8}]],\n\
 
 TEST (ParserTest, ArrayCorrectTest) {
     int result;
+    apus::ParserContext pctx;
 
     yy_scan_string(one_dimension_test_1);
-    result = yyparse();
+    result = yyparse(&pctx);
     EXPECT_EQ (result, 0);
 
     yy_scan_string(two_dimension_test_1);
-    result = yyparse();
+    result = yyparse(&pctx);
     EXPECT_EQ (result, 0);
 
     yy_scan_string(three_dimension_test_1);
-    result = yyparse();
+    result = yyparse(&pctx);
     EXPECT_EQ (result, 0);
 
     yy_scan_string(used_array_test_1);
-    result = yyparse();
+    result = yyparse(&pctx);
     EXPECT_EQ (result, 0);
 }
 
 TEST (ParserTest, ArrayCRTest) {
     int result;
+    apus::ParserContext pctx;
 
     yy_scan_string(three_dimension_test_3);
-    result = yyparse();
+    result = yyparse(&pctx);
     EXPECT_EQ (result, 0);
 }
