aff5c2693ba3e7fd63fb8a0fdcbef24a24c0f6c6
Kim Jung Hyun <junghyun153@naver.com>
2015-08-21 01:28:19 +0900

    parser: Make parser_context file
    Make new files
     - parser_context.cpp
     - parser_context.h
    
    Because yyparse() would deliver stmt_list pointer in parser_context object.

---
+++ include/parser_context.h
@@ -0,0 +1,23 @@
+#include <list>
+#include <memory>
+
+namespace apus {
+
+    class VirtualMachine;
+
+    class ParserContext {
+    public:
+        ParserContext(std::shared_ptr<VirtualMachine> vm = nullptr);
+        ~ParserContext();
+
+        std::shared_ptr<VirtualMachine> getVM();
+
+        void setVM(std::shared_ptr<VirtualMachine> vm);
+
+    private:
+
+        std::shared_ptr<VirtualMachine> vm_;
+
+    };
+
+}
