203725fd675fdb291e7d643ff8df3ed23286080c
ChanWoo Jeong <enghqii@gmail.com>
2015-08-22 04:25:25 +0900

    VM: Added CharacterValue
    CharacterValue를 추가했습니다.
    
    추가 하면서 Promote관련 규칙을 약간 수정했습니다.
    
    1. (상대방이) 같은 타입일때는 변환하지 않는다.
    
    2. 숫자 타입(Signed, Char, 그리고 앞으로 추가될 Unsigned 까지)과, 실수(Float) 타입이 만났을때는 무조건 실수 타입에 맞춘다.
    
    3. 숫자 타입끼리 만났을 경우, 바이트 사이즈가 큰 데이터 타입으로 변환한다. (C32 + S8 => C32, C16 + S64 => S64)
    
    4. 어떤 타입이든 문자열 타입을 만날경우, 문자열로 변환한다.

---
+++ src/ast/value/character_value.cpp
@@ -0,0 +1,186 @@
+#include "ast/value/character_value.h"
+#include "ast/value/signed_int_value.h"
+#include "ast/value/float_value.h"
+
+namespace apus {
+
+    std::shared_ptr<CharacterValue> CharacterValue::Create(TypeSpecifier type, int32_t value) {
+
+        if (type == C8 || type == C16 || type == C32) {
+            return std::shared_ptr<CharacterValue>(new CharacterValue(type, value));
+        }
+
+        return nullptr;
+    }
+
+    std::shared_ptr<Value> CharacterValue::Promote(
+            const std::shared_ptr<Value> another) const {
+
+        const TypeSpecifier another_type = another->getType();
+
+        if (type_ == another_type) {
+            return this->Copy();
+        }
+
+        switch (another_type) {
+
+            case C8:
+            case C16:
+            case C32:
+            case S8:
+            case S16:
+            case S32:
+            case S64: {
+
+                TypeSpecifier return_type = getSize() > another->getSize()
+                                            ? getType()
+                                            : another_type;
+
+                if (S8 <= return_type && return_type <= S64) {
+                    return SignedIntValue::Create(return_type, this->getCharValue());
+                }
+                else if (C8 <= return_type && return_type <= C32) {
+                    return CharacterValue::Create(return_type, this->getCharValue());
+                }
+
+                return nullptr;
+
+            }
+
+            case F32:
+            case F64: {
+                double double_value = static_cast<double>(this->getCharValue());
+                return FloatValue::Create(another_type, double_value);
+            }
+
+            default:
+                return nullptr;
+
+        }
+
+    }
+
+    std::shared_ptr<Value> CharacterValue::OperateBinary(
+            const Expression::Type expression_type,
+            const std::shared_ptr<Value> &right_promoted) const {
+
+        std::shared_ptr<Value> result = nullptr;
+
+        if (right_promoted->getType() == this->getType()) {
+
+            std::shared_ptr<CharacterValue> right_dynamic = std::dynamic_pointer_cast<CharacterValue>(right_promoted);
+
+            int32_t left_value = this->getCharValue();
+            int32_t right_value = right_dynamic->getCharValue();
+
+            int32_t result_value = 0;
+
+            switch (expression_type) {
+
+                case Expression::Type::EXP_OR :
+                    result_value = left_value | right_value;
+                    break;
+                case Expression::Type::EXP_AND :
+                    result_value = left_value & right_value;
+                    break;
+
+                case Expression::Type::EXP_EQL :
+                    result_value = left_value == right_value;
+                    break;
+                case Expression::Type::EXP_NEQ :
+                    result_value = left_value != right_value;
+                    break;
+                case Expression::Type::EXP_LSS :
+                    result_value = left_value < right_value;
+                    break;
+                case Expression::Type::EXP_GTR :
+                    result_value = left_value > right_value;
+                    break;
+                case Expression::Type::EXP_LEQ :
+                    result_value = left_value <= right_value;
+                    break;
+                case Expression::Type::EXP_GEQ :
+                    result_value = left_value >= right_value;
+                    break;
+
+                case Expression::Type::EXP_LSHIFT :
+                case Expression::Type::EXP_LSASSIGN :
+                    result_value = left_value << right_value;
+                    break;
+                case Expression::Type::EXP_RSHIFT :
+                case Expression::Type::EXP_RSASSIGN :
+                    result_value = left_value >> right_value;
+                    break;
+
+                case Expression::Type::EXP_ADD :
+                case Expression::Type::EXP_ADDASSIGN :
+                    result_value = left_value + right_value;
+                    break;
+                case Expression::Type::EXP_SUB :
+                case Expression::Type::EXP_SUBASSIGN :
+                    result_value = left_value - right_value;
+                    break;
+                case Expression::Type::EXP_MUL :
+                case Expression::Type::EXP_MULASSIGN :
+                    result_value = left_value * right_value;
+                    break;
+                case Expression::Type::EXP_DIV :
+                case Expression::Type::EXP_DIVASSIGN :
+                    result_value = left_value / right_value;
+                    break;
+                case Expression::Type::EXP_MOD :
+                case Expression::Type::EXP_MODASSIGN :
+                    result_value = left_value % right_value;
+                    break;
+                case Expression::Type::EXP_XOR :
+                case Expression::Type::EXP_XORASSIGN :
+                    result_value = left_value ^ right_value;
+                    break;
+
+                case Expression::Type::EXP_LOR :
+                    result_value = left_value || right_value;
+                    break;
+                case Expression::Type::EXP_LAND :
+                    result_value = left_value && right_value;
+                    break;
+
+                default:
+                    return nullptr;
+            }
+
+            result = CharacterValue::Create(this->getType(), result_value);
+        }
+
+        return result;
+    }
+
+    std::shared_ptr<Value> CharacterValue::OperateUnary(
+            const Expression::Type expression_type) const {
+
+        std::shared_ptr<Value> result = nullptr;
+        int32_t result_value = 0;
+
+        switch (expression_type) {
+            case Expression::Type::EXP_NOT :
+                result_value = !(this->getCharValue());
+                break;
+            case Expression::Type::EXP_REVERSE :
+                result_value = ~(this->getCharValue());
+                break;
+            case Expression::Type::EXP_SUB :
+                result_value = -(this->getCharValue());
+                break;
+            case Expression::Type::EXP_ADD :
+                result_value = +(this->getCharValue());
+                break;
+
+            default:
+                return nullptr;
+        }
+
+        result = CharacterValue::Create(this->getType(), result_value);
+
+        return result;
+    }
+
+}
