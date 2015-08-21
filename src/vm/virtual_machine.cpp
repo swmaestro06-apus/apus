ce64024c429d3adc07a81f0e468acdb3ced2652d
Kim Jung Hyun <junghyun153@naver.com>
2015-08-21 14:11:42 +0900

    parser: Added statement list in VM
    Added statement list, because VM will take stmt_list ptr.
    
    So, VM provide setStmtList(stmt_list), getStmtList() function.

--- src/vm/virtual_machine.cpp
+++ src/vm/virtual_machine.cpp
@@ -11,6 +11,14 @@ namespace apus {
 
     }
 
+    StmtListPtr VirtualMachine::getStmtList() {
+        return stmt_list_;
+    }
+
+    void VirtualMachine::setStmtList(StmtListPtr stmt_list) {
+        stmt_list_ = stmt_list;
+    }
+
     void VirtualMachine::Run() {
     }
 
