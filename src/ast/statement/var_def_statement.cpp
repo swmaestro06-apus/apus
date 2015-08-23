5743be0727799f3ed8f756dbe09000d2d5c96938
ChanWoo Jeong <enghqii@gmail.com>
2015-08-23 18:29:31 +0900

    VM: Updated 'VarDefStatement's constructors
    VarDefStatement의 생성자를 yacc에서 사용할 수 있게 확장하고, Execute도 추가했습니다 (아직 변수 테이블에 Insert하지 않음)

--- src/ast/statement/var_def_statement.cpp
+++ src/ast/statement/var_def_statement.cpp
@@ -1,10 +1,24 @@
 #include "ast/statement/var_def_statement.h"
+
+#include "ast/value/value.h"
+#include "ast/value/signed_int_value.h"
+#include "ast/value/float_value.h"
+
+#include "vm/variable_table.h"
 #include "vm/context.h"
 
 namespace apus {
 
-    VarDefStatement::VarDefStatement() {
+    VarDefStatement::VarDefStatement(TypeSpecifier type, std::string name)
+        : VarDefStatement(type, nullptr, name, nullptr) {
+    }
 
+    VarDefStatement::VarDefStatement(TypeSpecifier type, std::string name, Expression* initializer)
+        : VarDefStatement(type, nullptr, name, initializer) {
+    }
+
+    VarDefStatement::VarDefStatement(TypeSpecifier type, DataType* data_type, std::string name, Expression* initializer)
+        : type_(type), data_type_(data_type), name_(name), initializer_(initializer) {
     }
 
     VarDefStatement::~VarDefStatement() {
@@ -12,6 +26,20 @@ namespace apus {
     }
 
     void VarDefStatement::Execute(Context& context) {
+
+        std::shared_ptr<Variable> var = std::make_shared<Variable>(name_);
+
+        if (data_type_) {
+            var->setDataType(data_type_);
+        }
+
+        if (initializer_) {
+            var->setValue(initializer_->Evaluate(context));
+        }
+
+        else {
+            var->setValue(nullptr);
+        }
 
     }
 
