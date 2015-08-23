401d16dce54623bb8580d0bab51c7bfa092e32d3
ChanWoo Jeong <enghqii@gmail.com>
2015-08-23 18:29:31 +0900

    Stmt: Updated 'VarDefStatement's constructors
    VarDefStatement의 생성자를 yacc에서 사용할 수 있게 확장하고, Execute도 추가했습니다 (아직 변수 테이블에 Insert하지 않음)

--- src/ast/statement/var_def_statement.cpp
+++ src/ast/statement/var_def_statement.cpp
@@ -1,10 +1,40 @@
 #include "ast/statement/var_def_statement.h"
+
+#include "ast/value/value.h"
+
+#include "vm/variable_table.h"
 #include "vm/context.h"
 
 namespace apus {
 
-    VarDefStatement::VarDefStatement() {
+    VarDefStatement::VarDefStatement(TypeSpecifier type, std::string name) {
+        type_ = type;
+        type_name_ = "";
+        name_ = name;
+        initializer_ = nullptr;
+    }
 
+    VarDefStatement::VarDefStatement(TypeSpecifier type, std::string name, Expression* initializer) {
+        type_ = type;
+        type_name_ = "";
+        name_ = name;
+        initializer_ = ExprPtr(initializer);
+    }
+
+    VarDefStatement::VarDefStatement(std::string type_name,
+                                     std::string name) {
+        type_ = NOT_DEFINED;
+        type_name_ = type_name;
+        name_ = name;
+        initializer_ = nullptr;
+    }
+
+    VarDefStatement::VarDefStatement(std::string type_name, std::string name,
+                                     Expression *initializer) {
+        type_ = NOT_DEFINED;
+        type_name_ = type_name;
+        name_ = name;
+        initializer_ = ExprPtr(initializer);
     }
 
     VarDefStatement::~VarDefStatement() {
@@ -12,6 +42,23 @@ namespace apus {
     }
 
     void VarDefStatement::Execute(Context& context) {
+
+        std::shared_ptr<Variable> var = nullptr;
+
+        if (type_ != NOT_DEFINED) {
+            var = std::make_shared<Variable>(name_, context.FindDataType(type_));
+        }
+        else {
+            var = std::make_shared<Variable>(name_, context.FindDataType(type_name_));
+        }
+
+        if (initializer_) {
+            var->setValue(initializer_->Evaluate(context));
+        }
+
+        else {
+            var->setValue(nullptr);
+        }
 
     }
 
